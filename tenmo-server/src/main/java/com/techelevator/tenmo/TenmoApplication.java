902386c7e7d4d25070b56dda1c179300bd4830bb
Abdou Ndiaye <techdev@aol.com>
2022-03-09 12:15:26 -0500

    Implementing step 4

--- tenmo-server/src/main/java/com/techelevator/tenmo/TenmoApplication.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/TenmoApplication.java
@@ -13,9 +13,9 @@ public class TenmoApplication {
 
     public static void main(String[] args) {
 //        Account account = new Account(2001L, 1001L, new BigDecimal(1000));
-//        SpringApplication.run(TenmoApplication.class, args);
-//        System.out.println(account);
-//
+        SpringApplication.run(TenmoApplication.class, args);
+        //    System.out.println(account);
+
 //        User user = new User(1001L, "bkelley", "lol", true, new HashSet<>("ADMIN"))
     }
 
