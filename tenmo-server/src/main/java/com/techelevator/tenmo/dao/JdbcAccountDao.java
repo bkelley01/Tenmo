aa1c69f38aae3ad6a10796ad29fb10449ab94343
Virginia Anderson <virginia.j.schmidt@gmail.com>
2022-03-09 15:18:31 -0500

    additional DAO creation

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,61 +1,61 @@
-//package com.techelevator.tenmo.dao;
-//
-//import com.techelevator.tenmo.model.Account;
-//import com.techelevator.tenmo.model.User;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.support.rowset.SqlRowSet;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Component;
-//
-//import java.math.BigDecimal;
-//
-//@Component
-//public class JdbcAccountDao implements AccountDao  {
-//
-//    private JdbcTemplate jdbcTemplate;
-//
-//    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//    }
-//
-//    @Override
-//    public Account findAccountByUsername (String username) {
-//        Account account = null;
-//        String sql = "SELECT account_id " +
-//                "FROM account " +
-//                "JOIN tenmo_user ON tenmo_user.user_id = account.user_id " +
-//                "WHERE username = ?";
-//        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, username);
-//        if (rowSet.next()){
-//            account = mapRowToAccount(rowSet);
-//
-//        } return account;
-//        throw new UsernameNotFoundException("User " + username + " was not found.");
-//    }
-//
-//    @Override
-//    public Account getAccountBalanceByUsername (String username) {
-//        Account account = null;
-//        String sql = "SELECT balance " +
-//                "FROM account " +
-//                "JOIN tenmo_user ON tenmo_user.user_id = account.user_id " +
-//                "WHERE username = ?";
-//        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, username);
-//        if (rowSet.next()) {
-//            account = mapRowToAccount(rowSet);
-//        } return account;
-//        throw new UsernameNotFoundException("User " + username + " was not found.");
-//
-//    }
-//
-//
-//
-//    private Account mapRowToAccount(SqlRowSet rs) {
-//        Account account = new Account();
-//        account.setAccount_id(rs.getLong("account_id"));
-//        account.setBalance(rs.getBigDecimal("balance"));
-//        account.setUser_id(rs.getLong("user_id"));
-//        return account;
-//    }
-//
-//}
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class JdbcAccountDao implements AccountDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Account findAccountByUsername(String username) {
+        Account account = null;
+        String sql = "SELECT account_id " +
+                "FROM account " +
+                "JOIN tenmo_user ON tenmo_user.user_id = account.user_id " +
+                "WHERE username = ?";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, username);
+        if (rowSet.next()) {
+             account = mapRowToAccount(rowSet);
+        }
+        return account;
+       // throw new UsernameNotFoundException("User " + username + " was not found.");
+    }
+
+    @Override
+    public Account getAccountBalanceByUsername(String username) {
+        Account account = null;
+        String sql = "SELECT balance " +
+                "FROM account " +
+                "JOIN tenmo_user ON tenmo_user.user_id = account.user_id " +
+                "WHERE username = ?";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, username);
+        if (rowSet.next()) {
+            account = mapRowToAccount(rowSet);
+        }
+        return account;
+        // throw new UsernameNotFoundException("User " + username + " was not found.");
+
+    }
+
+
+    private Account mapRowToAccount(SqlRowSet rs) {
+        Account account = new Account();
+        account.setAccount_id(rs.getLong("account_id"));
+        account.setBalance(rs.getBigDecimal("balance"));
+        account.setUser_id(rs.getLong("user_id"));
+        return account;
+    }
+
+}
---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -0,0 +1,18 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Transfer;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcTransferDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTransferDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Transfer intiateNewTransfer(Transfer transfer) {
+        final String sql ="";
+    }
+
+}
